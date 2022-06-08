aa0a4b6b4dbef528a3d257edd9bbc1a89b4dc0a3
Vahid Mohammadi <vahidm1467@gmail.com>
2022-06-08 19:41:35 +0430

    feat(button-toggle): add ButtonToggle component with yes/no buttons

---
+++ src/ButtonToggle.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+interface props {
+  value?: boolean;
+  onValueChange: (value: boolean) => void;
+  disabled?: boolean;
+}
+
+const ButtonToggle: React.FC<props> = ({ onValueChange, value, disabled }) => {
+  return (
+    <div className="button-toggle">
+      <Button
+        type="button"
+        className={value !== true ? 'button-outlined' : ''}
+        onClick={() => onValueChange(true)}
+        disabled={disabled}
+      >
+        Yes
+      </Button>
+      <Button
+        type="button"
+        className={value !== false ? 'button-outlined' : ''}
+        onClick={() => onValueChange(false)}
+        disabled={disabled}
+      >
+        No
+      </Button>
+    </div>
+  );
+};
+
+ButtonToggle.propTypes = {
+  value: PropTypes.bool,
+  onValueChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+export default ButtonToggle;
