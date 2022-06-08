d6c784c804fb40adecb1985b68529df55b732914
Vahid Mohammadi <vahidm1467@gmail.com>
2022-06-08 19:46:09 +0430

    feat(check-list): add ability to toggle check list items with keyboard shortcuts

--- src/VerificationCheckItem.tsx
+++ src/VerificationCheckItem.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, KeyboardEventHandler, useState } from 'react';
 import { CheckListItem } from './models/check-list-item';
 import ButtonToggle from './ButtonToggle';
 import PropTypes from 'prop-types';
@@ -12,13 +12,21 @@ const VerificationCheckItem = React.forwardRef<HTMLLIElement, Props>(
   ({ checkItem: { id, description }, onAnswer }, ref) => {
     const [value, setValue] = useState<boolean | undefined>();
 
+    const onKeyDown: KeyboardEventHandler = event => {
+      if (event.key === '1') {
+        updateValue(true);
+      } else if (event.key === '2') {
+        updateValue(false);
+      }
+    };
+
     function updateValue(newValue: boolean) {
       setValue(newValue);
       onAnswer(newValue);
     }
     return (
       <Fragment>
-        <li id={'verification-check-item-' + id} ref={ref}>
+        <li id={'verification-check-item-' + id} ref={ref} tabIndex={-1} onKeyDown={onKeyDown}>
           <div className="verif-item-desc">{description}</div>
           <ButtonToggle onValueChange={updateValue} value={value} />
         </li>
